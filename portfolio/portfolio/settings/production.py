8d60d53ac98bc8646cb1b5c6e62fc74505e11d7a
Cam Barts <CameronD.Barts@gmail.com>
2020-12-06 22:33:41 -0500

    build(added-asgi-config): added asgi config, posgres db connection

--- portfolio/portfolio/settings/production.py
+++ portfolio/portfolio/settings/production.py
@@ -5,6 +5,22 @@ DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
+# DBHOST is only the server name, not the full URL
+hostname = os.environ["DBHOST"]
+
+# Configure Postgres database; the full username is username@servername,
+# which we construct using the DBHOST value.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DBNAME"],
+        "HOST": hostname + ".postgres.database.azure.com",
+        "USER": os.environ["DBUSER"] + "@" + hostname,
+        "PASSWORD": os.environ["DBPASS"],
+    }
+}
+
+
 try:
     from .local import *
 except ImportError:

02ed786cf80b00a99c4e85722b00320c89180c74
Cam Barts <CameronD.Barts@gmail.com>
2021-03-14 03:04:47 -0400

    style(style-changes): neon look, form overahul, added toasts
    after watching figma tutorials on youtube

--- portfolio/portfolio/static/js/portfolio.js
+++ portfolio/portfolio/static/js/portfolio.js
@@ -1,2 +1,21 @@
 const observer = lozad('.lazy');
 observer.observe();
+
+var neon = document.querySelectorAll('.left, .right')
+
+function setProperty(element, duration) {
+    element.style.setProperty('--animation-time', duration + 's');
+}
+
+function changeAnimationTime() {
+    for (i = 0; i < neon.length; i++) {
+        var animationDuration = Math.random() * 10;
+        setProperty(neon[i], animationDuration);
+    }
+}
+
+setInterval(changeAnimationTime, 1000);
+
+var x = document.getElementById("snackbar");
+x.className = "show";
+setTimeout(function() { x.className = x.className.replace("show", ""); }, 2900);
